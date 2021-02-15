b7caf592e3afb5c2bf1e82229cd6a5f986f7ae8c
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-02-15 17:50:32 -0500

    feat: Added Hero Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent],
+  declarations: [AppComponent, ToolbarComponent, HeroComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
