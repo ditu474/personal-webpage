a782568f7e4b63429da0581f33cedbedd411e6e9
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-02-15 19:44:46 -0500

    test: Added Mobile Menu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,10 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  /*   it(`should have as title 'personal-webpage'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-webpage');
-  }); */
 });
--- src/app/toolbar/toolbar.component.spec.ts
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -30,4 +30,41 @@ describe('ToolbarComponent', () => {
     const element: HTMLElement = fixture.nativeElement;
     expect(element.querySelector('.primary-nav-el')).toBeTruthy();
   });
+
+  it('should not show the mobile menu at start', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(element.querySelector('.mobile-menu')).toBeFalsy();
+  });
+
+  it('should have as showMobileMenu "false" at start', () => {
+    expect(component.showMobileMenu).toEqual(false);
+  });
+
+  it('should call mobileMenuHandler when menu item is clicked', () => {
+    const spy = spyOn(component, 'mobileMenuHandler');
+    const element = fixture.debugElement.nativeElement;
+    element.querySelector('.mobile-menu-icon').click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should change the value of showMobileMenu when mobileMenuHandler is called', () => {
+    component.mobileMenuHandler();
+    expect(component.showMobileMenu).toEqual(true);
+    component.mobileMenuHandler();
+    expect(component.showMobileMenu).toEqual(false);
+  });
+
+  it('should show the mobile menu if the menu item is clicked and hide if it is clicked again', () => {
+    const menuIcon = fixture.debugElement.nativeElement.querySelector(
+      '.mobile-menu-icon'
+    );
+    menuIcon.click();
+    const element: HTMLElement = fixture.nativeElement;
+    fixture.detectChanges();
+    expect(element.querySelector('.mobile-menu')).toBeTruthy();
+    menuIcon.click();
+    fixture.detectChanges();
+    expect(element.querySelector('.mobile-menu')).toBeFalsy();
+  });
 });
