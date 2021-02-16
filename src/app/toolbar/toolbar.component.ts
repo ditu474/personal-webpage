9b358930b34421b1afb1f113b2ce6fb9f302c886
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-02-15 19:44:35 -0500

    feat: Added Mobile Menu

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.css'],
 })
 export class ToolbarComponent implements OnInit {
+  showMobileMenu: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  mobileMenuHandler(): void {
+    this.showMobileMenu = !this.showMobileMenu;
   }
-
 }
